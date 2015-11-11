b316ca4234be712aa3768de27a90a52cba962d50
grigoriy-chirkov <grigoriy.chirkov@phystech.edu>
2015-11-11 19:04:33 +0300

    finished isaTable

--- func_sim/func_instr/disasm.cpp
+++ func_sim/func_instr/disasm.cpp
@@ -5,7 +5,27 @@
  */
 
 // Generic C
+#include <cassert>
 
 // Generic C++
 
 // MIPT-MIPS modules
+#include "func_instr.h"
+#include "types.h"
+#include "elf_parser.h"
+#include "func_memory.h"
+
+int main( int argc, const char* argv[])
+{
+    const int expected_arg_count = 2;
+    if ( argc != expected_arg_count + 1)
+    {
+        cout << "Wrong args" << endl;
+        exit(1);
+    }
+    const char* filename = argv[1];
+    const char* sect_name = argv[2];
+    vector<ElfSection> sections_array;
+    ElfSection::getAllElfSections( filename, sections_array);
+    
+}
--- func_sim/func_instr/func_instr.cpp
+++ func_sim/func_instr/func_instr.cpp
@@ -5,9 +5,36 @@
  */
 
 // Generic C
+#include <cassert>
 
 // Generic C++
+#include <string>
 
 // MIPT-MIPS modules
+#include "types.h"
+#include "elf_parser.h"
+#include "func_memory.h"
+#include "func_instr.h"
 
+using namespace std;
 
+FuncInstr::FuncInstr( uint32 bytes)
+{
+    this->bytes.raw = bytes;
+}
+
+FuncInstr::~FuncInstr()
+{
+    //nothing
+}
+
+string FuncInstr::Dump( string indent) const
+{
+    return string("Not implemented yet");
+}
+
+ostream& operator<<( ostream& out, const FuncInstr& instr)
+{
+    out << instr.Dump("");
+    return out;
+}
